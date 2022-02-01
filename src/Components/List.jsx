58a2a03a51572c8eed7f79fc7388c389389a4e89
ivailomihaylov <ivailo_mihaylov@icloud.com>
2022-02-01 22:57:59 +0200

    Drag and drop functionality

--- src/Components/List.jsx
+++ src/Components/List.jsx
@@ -1,25 +1,51 @@
 import React from "react";
 import Item from "./ListItem";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
 const List = ({ listItems, setListItems, saveLocalJobs }) => {
+  const handleOnDragEnd = (result) => {
+    if (!result.destination) {
+      return;
+    }
+    const items = Array.from(listItems);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+    setListItems(items);
+  };
   return (
     <div>
-      <ul className="list-group">
-        {listItems.map((item) => (
-          <Item
-            company={item.company}
-            position={item.position}
-            timestamp={item.timestamp}
-            image={item.image}
-            id={item.id}
-            item={item}
-            customNotes={item.customNotes}
-            setListItems={setListItems}
-            listItems={listItems}
-            key={item.id}
-            saveLocalJobs={saveLocalJobs}
-          />
-        ))}
-      </ul>
+      <DragDropContext onDragEnd={handleOnDragEnd}>
+        <Droppable droppableId="jobItems">
+          {(provided) => (
+            <ul
+              className="list-group jobItems"
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
+              {listItems.map((item, index) => (
+                <Item
+                  company={item.company}
+                  position={item.position}
+                  timestamp={item.timestamp}
+                  image={item.image}
+                  id={item.id}
+                  item={item}
+                  customNotes={item.customNotes}
+                  setListItems={setListItems}
+                  listItems={listItems}
+                  key={item.id}
+                  saveLocalJobs={saveLocalJobs}
+                  {...provided.draggableProps}
+                  {...provided.dragHandleProps}
+                  innerRef={provided.innerRef}
+                  index={index}
+                />
+              ))}
+              {provided.placeholder}
+            </ul>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 };
--- src/Components/ListItem.jsx
+++ src/Components/ListItem.jsx
@@ -3,6 +3,7 @@ import Note from "./Note";
 import React, { useContext } from "react";
 import { NotificationContext } from "./Notifications/NotificationProvider";
 import useCollapse from "react-collapsed";
+import { Draggable } from "react-beautiful-dnd";
 
 import { v4 } from "uuid";
 
@@ -16,6 +17,8 @@ const ListItem = ({
   setListItems,
   item,
   saveLocalJobs,
+  id,
+  index,
 }) => {
   const dispatch = useContext(NotificationContext);
   const { getCollapseProps, getToggleProps } = useCollapse();
@@ -41,45 +44,55 @@ const ListItem = ({
   };
   return (
     <div>
-      <li className=" d-flex justify-content-center p-1">
-        <div
-          className="job-list-item list-group-item list-group-item-action w-75 align-self-center d-flex justify-content-between p-0 rounded-left"
-          aria-current="true"
-          {...getToggleProps()}
-        >
-          <div className="list-img-container">
-            <img className="img-fluid" src={image} alt={company} />
-          </div>
-          <div className="position-title ms-3">
-            <h5 className="mb-2">{position}</h5>
-          </div>
-          <div className="d-flex ms-auto me-2 mt-1">
-            <small>
-              <i>Added {time}</i>
-            </small>
-          </div>
-        </div>
-
-        <button
-          className="btn btn-outline-danger btn-sm"
-          onClick={deleteHandler}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            className="bi bi-trash"
-            viewBox="0 0 16 16"
+      <Draggable key={id} draggableId={id} index={index}>
+        {(provided) => (
+          <li
+            key={id}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            ref={provided.innerRef}
+            className="d-flex justify-content-center p-1"
           >
-            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path>
-            <path
-              fillRule="evenodd"
-              d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
-            ></path>
-          </svg>
-        </button>
-      </li>
+            <div
+              className="job-list-item list-group-item list-group-item-action w-75 align-self-center d-flex justify-content-between p-0 rounded-left"
+              aria-current="true"
+              {...getToggleProps()}
+            >
+              <div className="list-img-container">
+                <img className="img-fluid" src={image} alt={company} />
+              </div>
+              <div className="position-title ms-3">
+                <h5 className="mb-2">{position}</h5>
+              </div>
+              <div className="d-flex ms-auto me-2 mt-1">
+                <small>
+                  <i>Added {time}</i>
+                </small>
+              </div>
+            </div>
+
+            <button
+              className="btn btn-outline-danger btn-sm"
+              onClick={deleteHandler}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-trash"
+                viewBox="0 0 16 16"
+              >
+                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path>
+                <path
+                  fillRule="evenodd"
+                  d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
+                ></path>
+              </svg>
+            </button>
+          </li>
+        )}
+      </Draggable>
       <div {...getCollapseProps()}>
         <div className="d-flex w-75 m-auto">
           {/* Pros section */}
